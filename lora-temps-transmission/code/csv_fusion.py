7a46e042e6b3cd624496301caf0babd2273b5d3d
Alexis <94443948+Helofy@users.noreply.github.com>
2022-01-25 16:11:59 +0100

    Add files via upload

--- lora-temps-transmission/code/csv_fusion.py
+++ lora-temps-transmission/code/csv_fusion.py
@@ -4,13 +4,11 @@ import glob
 import os
 
 
-# merging the files
+
 joined_files = os.path.join('U:\Documents\exel_python', "sending*.csv")
 
-# A list of all joined files is returned
 joined_list = glob.glob(joined_files)
 
-# Finally, the files are joined
 df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True)
 print(df)
-export_csv = df.to_csv ('Donnees.csv', index=None, header=True, encoding='utf-8', sep='\n')
+export_csv = df.to_csv ('Donnee.csv', index=None, header=True, encoding='utf-8', sep='\n')
